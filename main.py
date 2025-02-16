45599707aea75dd6cb5029fa6eface159e600f6f
Riley Weaver <c0wman1928@live.com>
2025-02-16 17:56:52 -0500

    Saved charts as PNG to charts folder.

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ for ticker in mytickers:
         plt.ylabel('Closing Price')
         plt.axis((9, 0, min_price, max_price ))
         plt.title(f" {ticker.upper()} last 10 days")
-        plt.show()
+        plt.savefig(f"charts/{ticker}.png")
     else:
         print(f"Do not have 10 days of data. Only have {len(last10days)} days.")
 
